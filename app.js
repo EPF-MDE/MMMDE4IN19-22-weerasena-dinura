60194e84265b802d082940fd0fc9cdf3c2ceff81
Dewmith <dewmithweerasena@gmail.com>
2024-04-23 10:39:16 +0200

    Changess

--- app.js
+++ app.js
@@ -12,17 +12,38 @@ const port = 3000
 
 // Define a route for the root URL ('/') that responds with 'Hello World!'
 app.get('/', (req, res) => {
-  res.send('Hello World!! It workss')
+  res.send('Hello World!!!')
 })
 
 app.get('/students', (req, res) => {
    const students = [
-    { name: "Dewmith WEERASENA", school: "EPF" },
+    { name: "Dewmith WEERE", school: "EPF" },
     { name: "Harry Potter", school: "Poudlard" }
   ];
 
   // Send the array of student information as the response
   res.send(students);
+});
+
+//Reading CSV
+app.get('/read-csv', (req, res) => {
+  // Define the file path
+  const filePath = __dirname + '/student_info.csv';
+
+  // Read the CSV file
+  fs.readFile(filePath, 'utf8', (err, data) => {
+    if (err) {
+      console.error('Error reading CSV file:', err);
+      res.status(500).send('Internal Server Error');
+      return;
+    }
+    
+    // Log the content of the CSV file
+    console.log('CSV File Content:');
+    console.log(data);
+
+    res.send('CSV file content logged to console.');
+  });
 });
 
 // Start the Express server and listen for incoming connections on the specified port
